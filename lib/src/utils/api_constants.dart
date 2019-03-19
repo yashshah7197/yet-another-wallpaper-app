12a32c8bcd2aa3964379b91c2a08379cd98fc878
yashshah7197 <yash.shah7197@live.com>
2019-03-20 02:45:41 +0530

    Add API constants

---
+++ lib/src/utils/api_constants.dart
@@ -0,0 +1,8 @@
+class ApiConstants {
+  static const baseUrl = 'https://api.unsplash.com';
+  static const accessKey = '';
+  static const requestHeaders = {
+    'Accept-Version': 'v1',
+    'Authorization': 'Client-ID $accessKey'
+  };
+}
