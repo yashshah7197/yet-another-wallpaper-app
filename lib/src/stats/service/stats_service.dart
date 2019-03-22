880d65ed0b5fc3e4a136beb3785cb60f34d21b7c
yashshah7197 <yash.shah7197@live.com>
2019-03-22 16:02:01 +0530

    Add stats API service

---
+++ lib/src/stats/service/stats_service.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:meta/meta.dart';
+
+import 'package:yet_another_wallpaper_app/src/stats/model/month_stats.dart';
+import 'package:yet_another_wallpaper_app/src/stats/model/total_stats.dart';
+import 'package:yet_another_wallpaper_app/src/utils/serializers.dart';
+
+class StatsService {
+  final Dio httpClient;
+
+  StatsService({@required this.httpClient}) : assert(httpClient != null);
+
+  Future<TotalStats> getTotalStats() async {
+    try {
+      final response = await httpClient.get('stats/total');
+      return deserialize<TotalStats>(response.data);
+    } on DioError catch (error) {
+      // TODO: Handle errors properly instead of returning stats with nulls
+      return TotalStats();
+    } catch (exception) {
+      // TODO: Handle errors apart from network and HTTP errors properly
+      return TotalStats();
+    }
+  }
+
+  Future<MonthStats> getMonthStats() async {
+    try {
+      final response = await httpClient.get('stats/month');
+      return deserialize<MonthStats>(response.data);
+    } on DioError catch (error) {
+      // TODO: Handle errors properly instead of returning stats with nulls
+      return MonthStats();
+    } catch (exception) {
+      // TODO: Handle errors apart from network and HTTP errors properly
+      return MonthStats();
+    }
+  }
+}
