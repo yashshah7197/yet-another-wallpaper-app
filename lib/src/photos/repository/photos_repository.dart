52ebbad13f990a883ac55328277384fbf10f9e83
yashshah7197 <yash.shah7197@live.com>
2019-03-22 20:40:24 +0530

    Add photos repository

---
+++ lib/src/photos/repository/photos_repository.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:built_collection/built_collection.dart';
+import 'package:meta/meta.dart';
+
+import 'package:yet_another_wallpaper_app/src/photos/model/photo.dart';
+import 'package:yet_another_wallpaper_app/src/photos/service/photos_service.dart';
+import 'package:yet_another_wallpaper_app/src/utils/api_constants.dart';
+
+class PhotosRepository {
+  final PhotosService photosService;
+
+  PhotosRepository({@required this.photosService})
+      : assert(photosService != null);
+
+  Future<BuiltList<Photo>> getPhotos(
+          {int page = 1,
+          int perPage = 30,
+          String sortOrder = PhotosSortOrder.latest}) async =>
+      await photosService.getPhotos(
+          page: page, perPage: perPage, sortOrder: sortOrder);
+
+  Future<Photo> getPhoto({@required String photoId}) async =>
+      await photosService.getPhoto(photoId: photoId);
+
+  Future<BuiltList<Photo>> getRandomPhotos({int count = 1}) async =>
+      await photosService.getRandomPhotos(count: count);
+
+  Future<bool> trackPhotoDownload({@required String photoId}) async =>
+      await photosService.trackPhotoDownload(photoId: photoId);
+}
