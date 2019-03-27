bc2dff5e7ca0f40d1ec0d11e23180f46998a78d2
yashshah7197 <yash.shah7197@live.com>
2019-03-28 00:30:24 +0530

    Add photos bloc

---
+++ lib/src/photos/bloc/photos_bloc.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'package:yet_another_wallpaper_app/src/photos/bloc/photos_event.dart';
+import 'package:yet_another_wallpaper_app/src/photos/bloc/photos_state.dart';
+import 'package:yet_another_wallpaper_app/src/photos/repository/photos_repository.dart';
+
+class PhotosBloc extends Bloc<PhotosEvent, PhotosState> {
+  final PhotosRepository _photosRepository;
+
+  int _currentPage = 1;
+
+  PhotosBloc(this._photosRepository) : assert(_photosRepository != null);
+
+  @override
+  PhotosState get initialState => PhotosEmptyState();
+
+  @override
+  Stream<PhotosState> mapEventToState(PhotosEvent currentEvent) async* {
+    if (currentEvent is PhotosFetchEvent) {
+      if (currentState is PhotosEmptyState) {
+        final photos = await _photosRepository.getPhotos(page: _currentPage);
+        _currentPage++;
+        yield PhotosLoadedState(photos: photos.toList());
+        return;
+      }
+      if (currentState is PhotosLoadedState) {
+        final photos = await _photosRepository.getPhotos(page: _currentPage);
+        _currentPage++;
+        yield PhotosLoadedState(
+            photos: List.from((currentState as PhotosLoadedState).photos)
+              ..addAll(photos));
+      }
+    }
+  }
+
+  @override
+  Stream<PhotosEvent> transform(Stream<PhotosEvent> events) {
+    return (events as Observable<PhotosEvent>)
+        .debounce(Duration(milliseconds: 500));
+  }
+}
