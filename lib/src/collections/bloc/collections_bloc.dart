e45a08fce1df60d09219b0723a2243c9844fda01
yashshah7197 <yash.shah7197@live.com>
2019-05-02 22:28:37 +0530

    Add collections bloc

---
+++ lib/src/collections/bloc/collections_bloc.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'package:yet_another_wallpaper_app/src/collections/bloc/collections_event.dart';
+import 'package:yet_another_wallpaper_app/src/collections/bloc/collections_state.dart';
+import 'package:yet_another_wallpaper_app/src/collections/repository/collections_repository.dart';
+
+class CollectionsBloc extends Bloc<CollectionsEvent, CollectionsState> {
+  final CollectionsRepository _collectionsRepository;
+
+  int _currentPage = 1;
+
+  CollectionsBloc(this._collectionsRepository)
+      : assert(_collectionsRepository != null);
+
+  @override
+  CollectionsState get initialState => CollectionsEmptyState();
+
+  @override
+  Stream<CollectionsState> mapEventToState(
+      CollectionsEvent currentEvent) async* {
+    if (currentEvent is CollectionsFetchEvent) {
+      if (currentState is CollectionsEmptyState) {
+        final collections =
+            await _collectionsRepository.getCollections(page: _currentPage);
+        _currentPage++;
+        yield CollectionsLoadedState(collections: collections.toList());
+        return;
+      }
+      if (currentState is CollectionsLoadedState) {
+        final collections =
+            await _collectionsRepository.getCollections(page: _currentPage);
+        _currentPage++;
+        yield CollectionsLoadedState(
+            collections:
+                List.from((currentState as CollectionsLoadedState).collections)
+                  ..addAll(collections));
+      }
+    }
+  }
+
+  @override
+  Stream<CollectionsState> transform(Stream<CollectionsEvent> events,
+      Stream<CollectionsState> next(CollectionsEvent event)) {
+    return super.transform(
+      (events as Observable<CollectionsEvent>).debounce(
+        Duration(milliseconds: 500),
+      ),
+      next,
+    );
+  }
+}
