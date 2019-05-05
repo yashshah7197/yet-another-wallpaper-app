c42b4cdb3e7b3a60dae48d43beb5c3f4433f764c
yashshah7197 <yash.shah7197@live.com>
2019-05-05 14:04:38 +0530

    Add filter app bar action for photos, collections and downloads

--- lib/src/home/route/home_route.dart
+++ lib/src/home/route/home_route.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:yet_another_wallpaper_app/src/collections/bloc/collections_bloc.dart';
 import 'package:yet_another_wallpaper_app/src/collections/bloc/collections_event.dart';
+import 'package:yet_another_wallpaper_app/src/collections/model/collections_sort_order.dart';
 import 'package:yet_another_wallpaper_app/src/collections/repository/collections_repository.dart';
 import 'package:yet_another_wallpaper_app/src/collections/route/collections_route.dart';
+import 'package:yet_another_wallpaper_app/src/downloads/model/downloads_sort_order.dart';
 import 'package:yet_another_wallpaper_app/src/downloads/route/downloads_route.dart';
 import 'package:yet_another_wallpaper_app/src/home/bloc/app_tab.dart';
 import 'package:yet_another_wallpaper_app/src/home/bloc/app_tab_bloc.dart';
@@ -11,6 +13,7 @@ import 'package:yet_another_wallpaper_app/src/home/bloc/app_tab_event.dart';
 import 'package:yet_another_wallpaper_app/src/home/widget/app_tab_selector.dart';
 import 'package:yet_another_wallpaper_app/src/photos/bloc/photos_bloc.dart';
 import 'package:yet_another_wallpaper_app/src/photos/bloc/photos_event.dart';
+import 'package:yet_another_wallpaper_app/src/photos/model/photos_sort_order.dart';
 import 'package:yet_another_wallpaper_app/src/photos/repository/photos_repository.dart';
 import 'package:yet_another_wallpaper_app/src/photos/route/photos_route.dart';
 
@@ -67,6 +70,9 @@ class _HomeRouteState extends State<HomeRoute> {
           child: Scaffold(
             appBar: AppBar(
               title: Text('Yet Another Wallpaper App'),
+              actions: <Widget>[
+                _buildAppBarActions(currentTab),
+              ],
             ),
             body: _buildBody(currentTab),
             bottomNavigationBar: AppTabSelector(
@@ -88,5 +94,103 @@ class _HomeRouteState extends State<HomeRoute> {
     } else if (currentTab == AppTab.downloads) {
       return DownloadsRoute();
     }
+  }
+
+  Widget _buildAppBarActions(AppTab currentTab) {
+    if (currentTab == AppTab.photos) {
+      return _buildPhotosAppBarActions();
+    }
+    if (currentTab == AppTab.collections) {
+      return _buildCollectionsAppBarActions();
+    }
+    if (currentTab == AppTab.downloads) {
+      return _buildDownloadsAppBarActions();
+    }
+  }
+
+  Widget _buildPhotosAppBarActions() {
+    return PopupMenuButton<PhotosSortOrder>(
+      icon: Icon(Icons.filter_list),
+      itemBuilder: (context) => <PopupMenuItem<PhotosSortOrder>>[
+            PopupMenuItem<PhotosSortOrder>(
+              child: Text('Latest'),
+              value: PhotosSortOrder.latest,
+            ),
+            PopupMenuItem<PhotosSortOrder>(
+              child: Text('Oldest'),
+              value: PhotosSortOrder.oldest,
+            ),
+            PopupMenuItem<PhotosSortOrder>(
+              child: Text('Popular'),
+              value: PhotosSortOrder.popular,
+            )
+          ],
+      onSelected: (photosSortOrder) {
+        if (photosSortOrder == PhotosSortOrder.latest) {
+          print('Photos Sort Order: Latest');
+        }
+        if (photosSortOrder == PhotosSortOrder.oldest) {
+          print('Photos Sort Order: Oldest');
+        }
+        if (photosSortOrder == PhotosSortOrder.popular) {
+          print('Photos Sort Order: Popular');
+        }
+      },
+    );
+  }
+
+  Widget _buildCollectionsAppBarActions() {
+    return PopupMenuButton<CollectionsSortOrder>(
+      icon: Icon(Icons.filter_list),
+      itemBuilder: (context) => <PopupMenuItem<CollectionsSortOrder>>[
+            PopupMenuItem<CollectionsSortOrder>(
+              child: Text('Latest'),
+              value: CollectionsSortOrder.latest,
+            ),
+            PopupMenuItem<CollectionsSortOrder>(
+              child: Text('Featured'),
+              value: CollectionsSortOrder.featured,
+            )
+          ],
+      onSelected: (collectionsSortOrder) {
+        if (collectionsSortOrder == CollectionsSortOrder.latest) {
+          print('Collections Sort Order: Latest');
+        }
+        if (collectionsSortOrder == CollectionsSortOrder.featured) {
+          print('Collections Sort Order: Featured');
+        }
+      },
+    );
+  }
+
+  Widget _buildDownloadsAppBarActions() {
+    return PopupMenuButton<DownloadsSortOrder>(
+      icon: Icon(Icons.filter_list),
+      itemBuilder: (context) => <PopupMenuItem<DownloadsSortOrder>>[
+            PopupMenuItem<DownloadsSortOrder>(
+              child: Text('All'),
+              value: DownloadsSortOrder.all,
+            ),
+            PopupMenuItem<DownloadsSortOrder>(
+              child: Text('In Progress'),
+              value: DownloadsSortOrder.inProgress,
+            ),
+            PopupMenuItem<DownloadsSortOrder>(
+              child: Text('Completed'),
+              value: DownloadsSortOrder.completed,
+            )
+          ],
+      onSelected: (downloadsSortOrder) {
+        if (downloadsSortOrder == DownloadsSortOrder.all) {
+          print('Downloads Sort Order: All');
+        }
+        if (downloadsSortOrder == DownloadsSortOrder.inProgress) {
+          print('Downloads Sort Order: In Progress');
+        }
+        if (downloadsSortOrder == DownloadsSortOrder.completed) {
+          print('Downloads Sort Order: Completed');
+        }
+      },
+    );
   }
 }
